1c3944e2085262dd27bbeda6da112265f9e40cd3
vinczematyas <matyas1vincze@gmail.com>
2024-05-15 13:14:31 +0200

    typo fix + data percentage arg

--- 0_sft.py
+++ 0_sft.py
@@ -15,11 +15,13 @@ from src.data import load_sft_dataset
 parser = argparse.ArgumentParser(description="Phase 0: Supervised Fine-Tuning.")
 parser.add_argument("-m", "--model_name", type=str, default="unsloth/tinyllama-bnb-4bit")
 parser.add_argument("-d", "--dataset_name", type=str, default="HuggingFaceH4/deita-10k-v0-sft")
+parser.add_argument("--data_percentage", type=int, default=1)
 parser.add_argument("--run_name", type=str, default="dev")
 parser.add_argument("--seed", type=int, default=420)
 parser.add_argument("--max_seq_length", type=int, default=2048)
 parser.add_argument("--lora_r", type=int, default=8)
 parser.add_argument("--lora_alpha", type=int, default=32)
+
 args = parser.parse_args()
 
 # Model and dataset dictionaries for easy access
@@ -36,7 +38,7 @@ args.output_dir = f"models/{model_dict[args.model_name]}/{args.run_name}/sft"
 
 # Load model and tokenizer + LORA
 model, tokenizer = load_model(args)
-peft_model = get_peft_model(model, args)
+peft_model = get_peft_model(args, model)
 
 # Load dataset
 dataset = load_sft_dataset(args, dataset_dict, percentage=1)
