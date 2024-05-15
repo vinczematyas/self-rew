5e2420add0f7c9cd4a08cf98c2f6b2fc8092f076
vinczematyas <matyas1vincze@gmail.com>
2024-05-15 13:11:28 +0200

    type fix

--- 0_sft.py
+++ 0_sft.py
@@ -25,7 +25,7 @@ args = parser.parse_args()
 # Model and dataset dictionaries for easy access
 model_dict = {
     "unsloth/tinyllama-bnb-4bit": "tinyllama",
-    "unsloth/llama-3-8b-bnb-4bit": "llama-8b".
+    "unsloth/llama-3-8b-bnb-4bit": "llama-8b",
 }
 dataset_dict = {
     "HuggingFaceH4/deita-10k-v0-sft": "deita-10k-v0-sft",
